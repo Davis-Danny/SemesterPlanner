e29e0432b9a7c4fe7b26999724370af5df96263b
Davis-Danny <ddude1282@gmail.com>
2015-06-19 09:47:11 -0600

    remove unneeded files and update .gitignore

--- validate_strings.js
+++ validate_strings.js
@@ -1,14 +1,14 @@
 function validateForm(name, time, days) {
     if (name == null || name == "") {
-        alert(“Invalid Input”);
+        alert("Invalid Input");
         return false;
     }
 	if (time == null || time == "") {
-        alert(“Invalid Input”);
+        alert("Invalid Input");
         return false;
     }
-	if (days != “MWF” || “mwf” || “TH” || “th” || “MW” || “mw” || “F” || “f”) {
-        alert(“Invalid Input. Enter using one letter for each day.”);
+	if (days != "MWF" || "mwf" || "TH" || "th" || "MW" || "mw" || "F" || "f") {
+        alert("Invalid Input. Enter using one letter for each day.");
         return false;
     }
 	return true;
