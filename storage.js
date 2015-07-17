92b26fd4f8a00096828fc250f167a7ac186b318d
Davis-Danny <ddude1282@gmail.com>
2015-07-17 14:29:21 -0600

    working prototype

--- storage.js
+++ storage.js
@@ -1,30 +1,111 @@
 var storage = {
-    //sections is an array to store all the sections we have collected so far
-    sections: [],
-    //this function returns an object that contains name, time, and days
-    newSection: function (name, time, days) {
-        var section = {name:name,time:time,days:days};
-        return section;
-    },
-    //adds a section to sections. Also performs rudimentary validation by checking that 
-    //  name, time, and days all have a value.
-    addSection: function (section) {
-        if(section.name === undefined || section.time === undefined || section.days === undefined){
-            output("section not valid");
-            return;
-        }
-        var sections = this.sections;
-        sections.push(section);
+	// sections is an array to store all the sections we have collected so far
+	sections : [],
+	events : [],
+	// this function returns an object that contains name, time, and days
+	newSection : function(name, startTime,endTime, days) {
+		var section = {
+			name : name,
+			startTime : startTime,
+			endTime: endTime,
+			days : days
+		};
+		return section;
+	},
+	// adds a section to sections. Also performs rudimentary validation by
+	// checking that
+	// name, time, and days all have a value.
+	addSection : function(section) {
+		if (section.name === undefined || section.time === undefined
+				|| section.days === undefined) {
+			output("section not valid");
+			return;
+		}
+		var sections = this.sections;
+		sections.push(section);
 
-    },
-    
-    // convenience function: creates a section with newSection(), then adds it with addSection()
-    // returns the created section
-    addNewSection: function (name, time, days) {
-        var section = this.newSection(name, time, days);
-        this.addSection(section);
-        return section;
-    }
+	},
 
-};
+	// convenience function: creates a section with newSection(), then adds it
+	// with addSection()
+	// returns the created section
+	addNewSection : function(name, time, days) {
+		var section = this.newSection(name, time, days);
+		this.addSection(section);
+		return section;
+	},
+}
+var CLIENT_ID = '740016804943-2ng58iqtp5eqngo478a41ahlsvu21n0q.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyCflYrSMh4IMMjggnrtFNOIaY3negH_Bs4'
+var SCOPES = [ "https://www.googleapis.com/auth/calendar" ];
+var event = null;
+function buildEvent(startTime, endTime, dayOffset, name) {
+	var day = 5 + dayOffset;
+	if (day < 10) {
+		day = '0' + day
+	}
+	/*
+	 * var event = { 'start' : { 'dateTime' : '2015-07-' + day + 'T' + startTime },
+	 * 'end' : { 'dateTime' : '2015-07-' + day + 'T' + endTime }, 'htmlLink':
+	 * 'google.com' }
+	 */
+	var event = {
+		'summary' : name,
+		'start' : {
+			'dateTime' : '2016-12-' + day + 'T' + startTime,
+			'timeZone' : 'America/Denver'
+		},
+		'end' : {
+			'dateTime' : '2016-12-' + day + 'T' + endTime,
+			'timeZone' : 'America/Denver'
+		},
+	};
+	return event;
+}
+function postEvent(event) {
+	this.event = event;
+	request = gapi.client.calendar.events.insert({
+		'calendarId' : 'primary',
+		'resource' : event
+	});
+	request.then(function(response) {
+		appendPre('Event created: ' + response.result.htmlLink);
+	}, function(reason) {
+		alert(reason.errors);
+	});
+}
 
+function postBuiltEvent(startTime, endTime, dayOffset, name) {
+	this.postEvent(this.buildEvent(startTime, endTime, dayOffset, name));
+}
+
+/*function postEvents(events) {
+	
+	events.forEach(function(event) {
+		postEvent(event);
+	})
+}*/
+function checkAuth() {
+	gapi.auth.authorize({
+		'client_id' : CLIENT_ID,
+		'scope' : SCOPES,
+		'immediate' : true
+	}, handleAuthResult);
+}
+function handleAuthResult(authResult) {
+	var authorizeDiv = document.getElementById('authorize-div');
+	if (authResult && !authResult.error) {
+		// Hide auth UI, then load client library.
+		authorizeDiv.style.display = 'none';
+		gapi.client.setApiKey(API_KEY);
+	} else {
+		// Show auth UI, allowing the user to initiate authorization by
+		// clicking authorize button.
+		authorizeDiv.style.display = 'inline';
+	}
+}
+function appendPre(message) {
+	var pre = document.getElementById('output');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+}
--- timeFormat.js
+++ timeFormat.js
@@ -1,29 +1,72 @@
+var x;
+var y;
+
 function timeFormat() {
 	// regular expression to match required time format
     re = /^\d{2}:\d{2}?$/;
+    re2 = /^\d{2}:\d{2}:\d{2}?$/;
     x = form.starttime.value
     y = form.endtime.value
 
-    if(!form.starttime.value.match(re)) {
-      alert("Invalid time format: " + form.starttime.value);
-      form.starttime.focus();
-      return false;
+    if(form.starttime.value.match(re)&&form.endtime.value.match(re)){
+    	x = form.starttime.value
+    	y = form.endtime.value
+    }else{
+	    if(!form.starttime.value.match(re)) {
+	      alert("Invalid time format: " + form.starttime.value);
+	      form.starttime.focus();
+	      return false;
+	    }
+	    else{
+	    	x = form.starttime.value + ":00"
+	    }
+	    
+	    if(!form.endtime.value.match(re)) {
+	        alert("Invalid time format: " + form.endtime.value);
+	        form.endtime.focus();
+	        return false;
+	      }
+	      else{
+	      	y = form.endtime.value + ":00"
+	      }
     }
-    else{
-    	x = form.starttime.value + ":00"
-    }
-    
-    if(!form.endtime.value.match(re)) {
-        alert("Invalid time format: " + form.endtime.value);
-        form.endtime.focus();
-        return false;
-      }
-      else{
-      	y = form.endtime.value + ":00"
-      }
-    
     form.starttime.value = x
     form.endtime.value = y
     
+    var days=[
+          document.getElementById("monBox"),
+          document.getElementById("tueBox"),
+          document.getElementById("wedBox"),
+          document.getElementById("thuBox"),
+          document.getElementById("friBox")];
+    gapi.client.load('calendar', 'v3',postEvents);
+    
+    
     return
+}
+function postEvents(){
+	// postBuiltEvent('14:00:00', '15:00:00', 0, 'CIT 380');
+	var i = 0;
+	var events = [];
+	var days=[
+	          document.getElementById("monBox"),
+	          document.getElementById("tueBox"),
+	          document.getElementById("wedBox"),
+	          document.getElementById("thuBox"),
+	          document.getElementById("friBox")];
+	var name = document.getElementById("name").value;
+	appendPre(x+","+y);
+		if(document.getElementById("monBox").checked){
+			postBuiltEvent(x+":00", y+":00", 0,name);
+		}
+		if(document.getElementById("tueBox").checked){
+			postBuiltEvent(x+":00", y+":00", 1,name);
+		}if(document.getElementById("wedBox").checked){
+			postBuiltEvent(x+":00", y+":00", 2,name);
+		}if(document.getElementById("thuBox").checked){
+			postBuiltEvent(x+":00", y+":00", 3,name);
+		}if(document.getElementById("friBox").checked){
+			postBuiltEvent(x+":00", y+":00", 4,name);
+		}
+		
 }
